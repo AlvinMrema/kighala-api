dd06ee1231ea3aefcdfb8fde48878d3981113d63
AlvinMrema <sonalpha023@gmail.com>
2023-12-18 12:58:48 +0300

    Added way to convert DB types to normal for 'Word' Struct

--- models.go
+++ models.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/AlvinMrema/kighala-api/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -13,12 +14,28 @@ type Word struct {
 	Word      string    `json:"word"`
 }
 
+func databaseWordToWord(dbWord database.Word) Word {
+	return Word{
+		ID: dbWord.ID,
+		CreatedAt: dbWord.CreatedAt,
+		UpdatedAt: dbWord.UpdatedAt,
+		Word: dbWord.Word,
+	}
+}
+
+func databaseWordsToWords(dbWords []database.Word) []Word {
+	words := []Word{}
+	for _, dbWord := range dbWords {
+		words = append(words, databaseWordToWord(dbWord))
+	}
+	return words
+}
 
 type Definition struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Definition string `json:"definition"`
-	PartOfSpeech string `json:"part_of_speech"`
-	WordID uuid.UUID `json:"word_id"`
+	ID           uuid.UUID `json:"id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	Definition   string    `json:"definition"`
+	PartOfSpeech string    `json:"part_of_speech"`
+	WordID       uuid.UUID `json:"word_id"`
 }
