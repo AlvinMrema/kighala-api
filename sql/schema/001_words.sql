d1f2f3130f5e2b81856b317373795d35652223c1
AlvinMrema <sonalpha023@gmail.com>
2023-12-20 09:56:47 +0300

    Modified word and definition fields to be Unique

--- sql/schema/001_words.sql
+++ sql/schema/001_words.sql
@@ -4,7 +4,7 @@ CREATE TABLE words (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    word TEXT NOT NULL
+    word TEXT UNIQUE NOT NULL
 );
 
 -- +goose Down
--- sql/schema/002_definitions.sql
+++ sql/schema/002_definitions.sql
@@ -4,7 +4,7 @@ CREATE TABLE definitions (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    definition TEXT NOT NULL,
+    definition TEXT UNIQUE NOT NULL,
     part_of_speech TEXT NOT NULL,
     word_id UUID NOT NULL REFERENCES words(id) ON DELETE CASCADE
 );
