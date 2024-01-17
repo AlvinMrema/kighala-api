1d495453c75b0ca65f44ed8244ffc5c8d9859dd4
AlvinMrema <sonalpha023@gmail.com>
2024-01-17 13:40:45 +0300

    removed godotenv for hosting

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ import (
 	"github.com/AlvinMrema/kighala-api/pkg/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
